60d8bbc4f13909facc62f64e8a77e49c7bc8ede8
thducuit <thducuit@gmail.com>
2016-09-15 03:44:43 +0000

    update and destroy photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,8 @@
 class PhotosController < ApplicationController
+  before_action :find_photo, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @photos = Photo.all
+    @photos = Photo.all.order("created_at DESC")
   end
 
   def new
@@ -8,26 +10,40 @@ class PhotosController < ApplicationController
   end
 
   def create
-    @photo = Photo.create(photo_params)
+    @photo = Photo.new(photo_params)
     if @photo.save
-      redirect_to photos_path
+      redirect_to @photo, notice: "Create Successfully !"
     else
       render 'new'
     end
   end
   
   def show
-    @photo = Photo.find(params[:id])
   end
 
   def edit
+    render 'edit'
   end
 
   def update
+    if @photo.update(photo_params)
+      redirect_to @photo, notice: "Update successfully !"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @photo.destroy
+    redirect_to root_path
   end
   
   private
+  def find_photo
+    @photo = Photo.find(params[:id])
+  end
+  
   def photo_params
-    params.require(:photo).permit(:title, :descriptio, :photo)
+    params.require(:photo).permit(:title, :description, :photo)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ActiveRecord::Base
   has_attached_file :photo, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:styles/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+
+  validates :title, presence: true
 end
